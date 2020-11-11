fab600b8f1373ce43d3cc65d8a9bfe66bdda4066
Matteo Agnoletto <30753195+EPMatt@users.noreply.github.com>
2020-11-11 17:16:39 +0100

    add support for additional reviewdog flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 python3 /usr/local/bin/converter.py $GITHUB_WORKSPACE/${INPUT_LINT_XML_FILE}
-cat output_checkstyle.xml | reviewdog -f=checkstyle -name="Android Lint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
+cat output_checkstyle.xml | reviewdog -f=checkstyle -name="Android Lint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}" ${INPUT_REVIEWDOG_FLAGS}
